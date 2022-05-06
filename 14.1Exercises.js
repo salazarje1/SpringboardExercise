8fb5f0ffe54e1e0b27b671256a510cae85137165
Josue Salazar <salazar@Josues-MBP.localdomain>
2022-05-06 10:49:29 -0700

    adding exercise for 14.1

---
+++ 14.1Exercises.js
@@ -0,0 +1,37 @@
+// Part One: Solidify Terminology
+    // In your own terms, define the following terms:
+
+    // What is HTTP?
+    // HTTP is protocol in the application layer, stand for hypertext transfer protocol.
+
+    // What is a URL?
+    // is string of characters used to find pages in the browser, replace ip addresses, add query strings, and more. 
+
+    // What is DNS?
+    // Domain Name Service or servers are used to find the ip address of the webpage your seaching for. 
+
+    // What is a query string?
+    // goes at the end of a get request. Key/value pair used to send extra information in the URL
+
+    // What are two HTTP verbs and how are they different?
+    // Get and Post 
+        // Get - is used to retrieve data without change anythin g
+        // Post - is used to change things or modify. 
+
+    // What is an HTTP request?
+    // a request is what the browser sends the server, asking for information or files.
+
+    // What is an HTTP response?
+    // a response is what the server sends back to the browser, with the information requested  
+
+    // What is an HTTP header? Give a couple examples of request and response headers you have seen.
+    // an HTTP header is where extra information is sent, either about the request or response or about the device itself
+        // a couple examples are: cookies, language, content type, 
+
+    // What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+        // First the browser sends a request looking for the ip address for the website 
+        // Then send another request to the server with the ip address replacing the name 
+        // The server gets the request and looks for the information being requested 
+        // Gets a response ready and send it to the broswer 
+
+        // These are very simple steps, it is much much more complicated. 
